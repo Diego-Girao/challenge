c45d56314bc28bfe20d1f7d340c2091475e18636
diegogirao <diegoramiro25@gmail.com>
2023-06-01 15:57:22 +0100

    feat: new app component created

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+
+import Restaurants from "./src/pages/Restaurants"
+import RestaurantDetails from "./src/pages/RestaurantDetails"
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Restaurants" component={Restaurants} />
+				<Stack.Screen name="RestaurantDetails" component={RestaurantDetails} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
