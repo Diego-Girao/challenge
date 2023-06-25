8ca103d6ecc13daa5e0a90492dafe673ae729739
diegogirao <diegoramiro25@gmail.com>
2023-06-25 13:03:49 +0100

    Update: import persistor and persistgate

--- App.js
+++ App.js
@@ -6,22 +6,27 @@ import { createStackNavigator } from "@react-navigation/stack"
 import Restaurants from "./src/pages/Restaurants"
 import RestaurantDetails from "./src/pages/RestaurantDetails"
 import { Provider } from "react-redux"
-import store from "./src/Redux/store"
+
+import { PersistGate } from "redux-persist/integration/react"
+
+import { store, persistor } from "./src/redux/store"
 
 const Stack = createStackNavigator()
 
 export default function App() {
 	return (
 		<Provider store={store}>
-			<NavigationContainer>
-				<Stack.Navigator>
-					<Stack.Screen name="Restaurants" component={Restaurants} />
-					<Stack.Screen
-						name="RestaurantDetails"
-						component={RestaurantDetails}
-					/>
-				</Stack.Navigator>
-			</NavigationContainer>
+			<PersistGate loading={null} persistor={persistor}>
+				<NavigationContainer>
+					<Stack.Navigator>
+						<Stack.Screen name="Restaurants" component={Restaurants} />
+						<Stack.Screen
+							name="RestaurantDetails"
+							component={RestaurantDetails}
+						/>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</PersistGate>
 		</Provider>
 	)
 }
